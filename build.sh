e0c734867e605ea20d3245c97bf8add009efcd53
Ilya Strukov <iley@iley.ru>
2023-10-10 08:31:09 +0300

    Switch to go modules

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -u -e -x
-GOOS=linux GOARCH=arm GOARM=5 go build
+GOOS=linux GOARCH=arm GOARM=5 go build -mod=vendor
 tar -czvf ev3-remote.tar.gz ev3-remote static
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/iley/ev3-remote
+
+go 1.20
+
+require golang.org/x/net v0.16.0
